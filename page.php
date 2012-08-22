2c961d37ebfee6e1b3b1a385187dfc98d237e3d2
AngeZanetti <ange.zanetti@gmail.com>
2012-08-22 16:25:15 +0200

    Change h2 to h1 for SEO

--- page.php
+++ page.php
@@ -10,7 +10,7 @@ get_header(); ?>
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<article class="post" id="post-<?php the_ID(); ?>">
   
-			<h2><?php the_title(); ?></h2>
+			<h1><?php the_title(); ?></h1>
 
 			<div class="entry">
 
--- single.php
+++ single.php
@@ -9,7 +9,7 @@ get_header(); ?>
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
     
-      <h2 class="entry-title"><a title="<?php the_title(); ?>" href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+      <h1 class="entry-title"><a title="<?php the_title(); ?>" href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
 
         <cite class="entry-meta">Publié le <time datetime="<?php echo date(DATE_W3C); ?>" pubdate class="updated"><?php the_time('j F, Y') ?></time>  • <?php the_tags(); ?> </cite>
 
